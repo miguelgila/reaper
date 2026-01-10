c6573fe76a0c909f0a126f4044d23d0b46e6edf8
Miguel Gila <miguel.gila@cscs.ch>
2026-01-10 14:22:33 +0100

    fix: remove src/ from gitignore, add src/main.rs, remove Windows from CI

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Welcome to reaper!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
